66aacf9601e5ea8faeee5eacc3037f0791b575d2
n-pall <npall@thesummitgrp.com>
2025-06-13 12:17:02 +0530

    add g branch -D

--- config.fish
+++ config.fish
@@ -22,6 +22,7 @@ alias grba="git rebase --abort"
 alias grbc="git rebase --continue"
 alias gcp="git cherry-pick"
 alias gfo="git fetch origin"
+alias gbd="git branch -D"
 
 
 alias pip=pip3
@@ -45,3 +46,7 @@ set --export PATH $BUN_INSTALL/bin $PATH
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
